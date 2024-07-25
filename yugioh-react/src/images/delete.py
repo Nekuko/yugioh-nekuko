6ab3ac8dcc9ad887acbaa75d7ab0bb32e93ab2a9
Nekuko <21350635+Nekuko@users.noreply.github.com>
2024-07-25 13:53:29 +0100

    Delete images

--- yugioh-react/src/images/delete.py
+++
@@ -1,9 +0,0 @@
-import os
-from os import listdir
-from os.path import isfile, join
-import time
-onlyfiles = [f for f in listdir("./") if isfile(join("./", f))]
-
-for f in onlyfiles:
-    if "cropped" in f:
-        os.remove(f)
